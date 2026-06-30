e506c236706c7b511a20a95eb709fa479a787a21
itsmesafik04 <itsmesafik04@gmail.com>
2026-06-30 08:29:16 +0530

    Updated Database Configuration

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Gullyboy04@localhost/bookstore_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/bookstore_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
